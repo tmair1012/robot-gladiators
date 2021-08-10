2553d2a6340cca764533b0d6a0b3714e3725b1de
Tyler <tyleramair@gmail.com>
2021-08-10 15:32:33 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,30 @@
-var playerName = "Tony The Robot ";
+
+var playerName = window.prompt("What is your robot's Name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames.length);
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
 
 // this is to create a function
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that htey are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -44,7 +57,7 @@ var fight = function () {
         window.alert(playerName + " has died!");
     }
     else {
-        window.alert(playerName + " still has " + palyerHealth + " health left.");
+        window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     // if player chooses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
@@ -73,4 +86,7 @@ else {
 
 
 // your have to execute the function like this
-fight();
+//fight();
+for (var i = 0; i< enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
